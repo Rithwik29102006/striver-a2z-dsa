1ce16c144f7cd65684571a15a93b5dee851c5534
Rithwik29102006 <rithwikreddy1029@gmail.com>
2026-01-15 18:25:21 +0000

    quick sort

---
+++ Sorting/quicksort.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+
+        def partition(nums, low, high):
+            pivot = nums[high]     # correct: nums[high]
+            i = low - 1
+
+            for j in range(low, high):
+                if nums[j] <= pivot:
+                    i += 1
+                    nums[i], nums[j] = nums[j], nums[i]
+
+            nums[i+1], nums[high] = nums[high], nums[i+1]
+            return i + 1
+
+        def quick_sort(nums, low, high):
+            if low < high:
+                pi = partition(nums, low, high)
+                quick_sort(nums, low, pi - 1)
+                quick_sort(nums, pi + 1, high)
+
+        quick_sort(nums, 0, len(nums) - 1)
+        return nums
