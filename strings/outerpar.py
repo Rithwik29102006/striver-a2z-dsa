adb97251917acf9c30dbb7900299daa1861f7dfb
Rithwik29102006 <rithwikreddy1029@gmail.com>
2026-02-07 17:41:06 +0000

    remove outer most paranthesis

---
+++ strings/outerpar.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        depth = 0
+        result = []
+
+        for ch in s:
+            if ch == '(':
+                if depth > 0:
+                    result.append(ch)
+                depth += 1
+            else:  # ch == ')'
+                depth -= 1
+                if depth > 0:
+                    result.append(ch)
+
+        return "".join(result)
