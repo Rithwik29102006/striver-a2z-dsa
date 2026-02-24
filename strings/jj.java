f94616dfd732b5bddcc24ef7afd122ad5ef71eb7
Rithwik29102006 <rithwikreddy1029@gmail.com>
2026-02-24 16:35:08 +0000

    new

--- strings/jj.java
+++ strings/jj.java
@@ -3,7 +3,4 @@ class Solution {
         // Copy value of next node
         node.val = node.next.val;
 
-        // Bypass the next node
-        node.next = node.next.next;
-    }
-}
+        // Bypass the next no}
