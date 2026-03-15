cc23f7dab972fb59bb256e2ca5983fb56426937c
Thoomu Rithwik Reddy <rithwikreddy1029@gmail.com>
2026-03-15 19:32:20 +0530

    Update jj.java

--- strings/jj.java
+++ strings/jj.java
@@ -1,1 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         int xorr = 0;
 
+        // XOR all elements — duplicates cancel each other out
+        for (int num1 : nums) {
+            xorr ^= num1;
+        }
+
+        return xorr;
+    }
+}
