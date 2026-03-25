be896644663e459d851a40504f74a57b9bd6957d
Thoomu Rithwik Reddy <rithwikreddy1029@gmail.com>
2026-03-25 23:48:37 +0530

    Update jj.java

--- strings/jj.java
+++ strings/jj.java
@@ -3,9 +3,6 @@ class Solution {
         int low = 0, mid = 0, high = nums.length - 1;
 
         while (mid <= high) {
-            if (nums[mid] == 0) {
-                // swap nums[mid] and nums[low]
-                int temp = nums[mid];
-            
+            if (nums[mid
                 nums[low] = temp;
 
