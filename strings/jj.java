9f82424a84f74519e8e97a2fc324635b28fa508a
Thoomu Rithwik Reddy <rithwikreddy1029@gmail.com>
2026-03-26 23:30:02 +0530

    Update jj.java

--- strings/jj.java
+++ strings/jj.java
@@ -1,8 +1,1 @@
-class Solution {
-    public void sortColors(int[] nums) {
-        int low = 0, mid = 0, high = nums.length - 1;
-
-        while (mid <= high) {
-            if (nums[mid
-                nums[low] = temp;
 
