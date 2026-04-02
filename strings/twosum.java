e6403263f64187d505024fa22dba41048537399b
Thoomu Rithwik Reddy <rithwikreddy1029@gmail.com>
2026-04-02 23:49:58 +0530

    Update twosum.java

--- strings/twosum.java
+++ strings/twosum.java
@@ -1,20 +1,4 @@
-import java.util.HashMap;
-
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < nums.length; i++) {
-            
-            int complement = target - nums[i];
-            
-            if(map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
-            }
-            
-            map.put(nums[i], i);
-        }
+import java.
         
         return new int[]{};
     }
