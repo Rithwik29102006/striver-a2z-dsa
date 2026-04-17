f8a3f65c0b78f96c852beda6d20cbd5a284e0c8f
Thoomu Rithwik Reddy <rithwikreddy1029@gmail.com>
2026-04-17 23:06:47 +0530

    Create pascal1.py

---
+++ array/pascal1.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+
+        for i in range(numRows):
+            # start each row with 1s
+            row = [1] * (i + 1)
+
+            # fill middle values
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+            triangle.append(row)
+
+        return triangle
