0c7e325fabda1746c2db6c3c1cf21ba911a3c8f8
Thoomu Rithwik Reddy <rithwikreddy1029@gmail.com>
2026-05-11 23:22:29 +0530

    Update k.java

--- binarysearch/k.java
+++ binarysearch/k.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
 
+        // Binary search loop
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // Number of missing elements before index mid
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                low = mid + 1;  // Move right
+            } else {
+                high = mid - 1; // Move left
+            }
+        }
+
+        // Final result after binary search
+        return k + high + 1;
+    }
+    }
