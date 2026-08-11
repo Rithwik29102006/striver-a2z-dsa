ae6195d4fae002f27f5dc9cae10a2e3594d556b5
Thoomu Rithwik Reddy <rithwikreddy1029@gmail.com>
2026-08-11 22:56:18 +0530

    Implement pivot index calculation in pivotindex.java

---
+++ array2/basic/pivotindex.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+        int totalSum = 0;
+        int leftSum = 0;
+
+        // Calculate total sum
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        // Find pivot
+        for (int i = 0; i < nums.length; i++) {
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
