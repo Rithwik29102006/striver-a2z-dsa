8892e2404b53fb63924784ba95faff93e94184ba
Thoomu Rithwik Reddy <rithwikreddy1029@gmail.com>
2026-08-14 22:30:42 +0530

    Implement findMaxLength method in contiguousarr.java

---
+++ array2/basic/contiguousarr.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // prefix sum 0 is considered to appear before the array starts
+        map.put(0, -1);
+
+        int count = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0) {
+                count--;
+            } else {
+                count++;
+            }
+
+            if (map.containsKey(count)) {
+
+                int previousIndex = map.get(count);
+
+                int length = i - previousIndex;
+
+                maxLength = Math.max(maxLength, length);
+
+            } else {
+
+                // store only the first occurrence
+                map.put(count, i);
+            }
+        }
+
+        return maxLength;
+    }
+}
